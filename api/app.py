bb4020805e25f08922aee079bdf0db2c2f3f9e79
mister-rafael <93235866+mister-rafael@users.noreply.github.com>
2025-05-25 19:55:27 -0300

    Update app.py
    Testando se irá funcionar colocando o "request". Foi colocado também um comentário.

--- api/app.py
+++ api/app.py
@@ -1,11 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
+# Cria a instância do Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def home(): # Rota para a página inicial index.html
     return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return 'About'
