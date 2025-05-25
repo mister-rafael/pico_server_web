520672663b9ff5e04691202b781969364bfd7474
mister-rafael <93235866+mister-rafael@users.noreply.github.com>
2025-05-25 20:17:21 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,8 +1,19 @@
+# Importa as bibliotecas Flask e SocketIO
 from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, send
 
 # Cria a instância do Flask
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
-def home(): # Rota para a página inicial index.html
-    return render_template('index.html')
+def index():
+    return "Servidor Flask-SocketIO está no ar!"
+
+@socketio.on('mensagem')
+def handle_message(data):
+    print(f"Mensagem recebida: {data}")
+    emit('resposta', {'resposta': 'Recebido com sucesso!'})
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=10000)
