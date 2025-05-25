7a6cdb82d3cc7f8aaca9494d1bfe74c90966b9d8
mister-rafael <93235866+mister-rafael@users.noreply.github.com>
2025-05-25 20:27:57 -0300

    Update

--- api/app.py
+++ api/app.py
@@ -16,4 +16,4 @@ def handle_message(data):
     emit('resposta', {'resposta': 'Recebido com sucesso!'})
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=10000)
+    socketio.run(app, host='0.0.0.0', port=5000)
